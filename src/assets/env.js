0928368b24ceac380220153e58e3e3b8d860d73a
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2024-05-07 16:47:40 +0200

    Release 0.1.0 image of ui
    Issue-ID: PORTALNG-97
    Change-Id: I1955ba9a5f352b9ffa8d68b5e915da3ef6c84991
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

--- src/assets/env.js
+++ src/assets/env.js
@@ -7,5 +7,5 @@
   window["env"]["keycloak"]["realm"];
   window["env"]["keycloak"]["clientId"];
   window["env"]["customStyleEnabled"];
-  window["env"]["loggingEnabled"]
+  window["env"]["loggingEnabled"];
 })(this);
